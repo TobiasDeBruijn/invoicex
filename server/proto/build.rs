a4a0fc9f62d4ee1a01bad127ddaaf6532934c2a7
Tobias de Bruijn <t.debruijn@array21.dev>
2022-06-21 18:09:31 +0200

    Initial server commit

---
+++ server/proto/build.rs
@@ -0,0 +1,32 @@
+use std::{fs, io};
+use std::path::{Path, PathBuf};
+
+fn main() -> io::Result<()> {
+    println!("cargo:rerun-if-changed=./proto");
+
+    std::env::set_var("PROTOC_NO_VENDOR", "");
+
+    let protos = find_protos(Path::new("./proto"))?;
+    println!("Done: {protos:?}");
+    prost_build::compile_protos(&protos, &["./proto"])?;
+
+    Ok(())
+}
+
+fn find_protos(dir: &Path) -> io::Result<Vec<PathBuf>> {
+    let mut result = Vec::new();
+
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        println!("F: {:?}", entry.path());
+
+        if entry.file_type()?.is_dir() {
+            result.append(&mut find_protos(&entry.path())?);
+        } else if entry.file_type()?.is_file() && entry.file_name().to_str().unwrap().ends_with(".proto") {
+            println!("Found proto file!");
+            result.push(entry.path())
+        }
+    }
+
+    Ok(result)
+}
---
+++ server/proto/src/lib.rs
@@ -0,0 +1,7 @@
+pub mod entities {
+    include!(concat!(env!("OUT_DIR"), "/dev.array21.invoicex.entities.rs"));
+}
+
+pub mod payloads {
+    include!(concat!(env!("OUT_DIR"), "/dev.array21.invoicex.payloads.rs"));
+}
